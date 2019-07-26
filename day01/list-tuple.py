5d3733796ea15378091185cb53491abea93b999b
chenwenjie707 <1053702190@qq.com>
2019-07-26 10:25:41 +0800

    practice

---
+++ day01/list-tuple.py
@@ -0,0 +1,18 @@
+# -*- coding:UTF-8 -*- #
+classmates = ['Michael', 'Bob', 'Tracy']
+print (classmates[0])
+print (classmates[-1])
+
+classmates.append('wenjie')
+print (classmates)
+
+
+classmates.insert(1, u'丹丹')
+print (classmates)
+
+
+classmates.pop()
+print (classmates)
+
+classmates.pop(0)
+print (classmates)
